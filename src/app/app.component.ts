9bd097f830c984a22a6e80857dbf090e6ea12d3b
Red Chien <redchien@viridiasolutions.com>
2023-02-01 22:29:39 +0800

    almost done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,178 @@
-import { Component } from '@angular/core';
+import { ThisReceiver } from '@angular/compiler';
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
+import { ChatService } from './sevices/chat/chat.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
+  @ViewChild('popup', { static: false }) popup: any;
+
   title = 'chat-client';
+
+  public roomId: string;
+  public messageText: string;
+  public messageArray: { user: string; message: string }[] = [];
+  private storageArray = []
+
+  public showScreen: boolean;
+  public phone: string;
+  public currentUser;
+  public selectedUser;
+
+  public userList = [
+    {
+      id: 1,
+      name: 'Red',
+      phone: '0961336838',
+      image: 'assets/user/user-1.png',
+      roomId: {
+        2: 'room-1',
+        3: 'room-2',
+        4: 'room-3',
+      },
+    },
+    {
+      id: 2,
+      name: 'Red2',
+      phone: '0961336839',
+      image: 'assets/user/user-2.png',
+      roomId: {
+        1: 'room-1',
+        3: 'room-4',
+        4: 'room-5',
+      },
+    },
+    {
+      id: 3,
+      name: 'Red3',
+      phone: '0961336837',
+      image: 'assets/user/user-3.png',
+      roomId: {
+        1: 'room-2',
+        2: 'room-4',
+        4: 'room-6',
+      },
+    },
+    {
+      id: 4,
+      name: 'Red4',
+      phone: '0961336836',
+      image: 'assets/user/user-4.png',
+      roomId: {
+        1: 'room-3',
+        2: 'room-5',
+        3: 'room-6',
+      },
+    },
+  ];
+
+  constructor(
+    private modalService: NgbModal,
+    private chatService: ChatService
+  ) {}
+
+  ngOnInit(): void {
+    this.chatService.getMessage()
+      .subscribe((data: { user: string, message: string }) => {
+      // this.messageArray.push(data);
+      console.log('this.roomId :>> ', this.roomId);
+      if(this.roomId){
+        setTimeout(() => {
+           this.storageArray = this.chatService.getStorage();
+           const storeIndex = this.storageArray.findIndex((storage) => {
+             return storage.roomId === this.roomId;
+           });
+           this.messageArray = this.storageArray[storeIndex].chats;
+        }, 500)
+      }
+    });
+  }
+
+  ngAfterViewInit(): void {
+    this.openPopup(this.popup);
+  }
+
+  openPopup(content: any) {
+    this.modalService.open(content, { backdrop: 'static', centered: true });
+  }
+
+  login(dismiss: any) {
+    console.log('hello')
+    this.currentUser = this.userList.find(
+      (user) => {
+        console.log('user :>> ', user.phone);
+        console.log('this.phone :>> ', this.phone);
+        return user.phone === this.phone.toString()
+      }
+    );
+    this.userList = this.userList.filter(
+      (user) => user.phone !== this.phone.toString()
+    );
+      console.log('this.currentUser :>> ', this.currentUser);
+    if (this.currentUser) {
+      this.showScreen = true;
+      dismiss();
+    }
+  }
+
+  selectUserHandler(phone: string) {
+    console.log('select User phone :>> ', phone);
+    this.selectedUser = this.userList.find((user) => user.phone === phone);
+    this.roomId = this.selectedUser.roomId[this.currentUser.id];
+    console.log('this.roomId :>> ', this.roomId);
+    this.messageArray = [];
+
+    this.storageArray = this.chatService.getStorage()
+    console.log('this.storageArray :>> ', this.storageArray);
+    const storeIndex = this.storageArray.findIndex(storage => {
+      return storage.roomId === this.roomId
+    })
+
+    this.join(this.currentUser.name, this.roomId);
+  }
+
+  join(username: string, roomId: string) {
+    this.chatService.joinRoom({ user: username, room: roomId });
+  }
+
+  sendMessage() {
+    this.chatService.sendMessage({
+      user: this.currentUser.name,
+      room: this.roomId,
+      message: this.messageText,
+    });
+
+    this.storageArray = this.chatService.getStorage()
+    const storeIndex = this.storageArray.findIndex(storage => {
+      return storage.roomId === this.roomId
+    })
+
+    if(storeIndex > -1){
+      this.storageArray[storeIndex].chats.push({
+        user: this.currentUser.name,
+        message: this.messageText
+      })
+    }else{
+      const updateStorage = {
+        roomId: this.roomId,
+        chats: [{
+          user: this.currentUser.name,
+          message: this.messageText
+        }]
+      }
+
+      this.storageArray.push(updateStorage)
+    }
+
+    this.chatService.setStorage(this.storageArray)
+    this.messageText = '';
+  }
+
+  test(){
+    console.log('button click')
+  }
 }
---
+++ src/app/sevices/chat/chat.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { io, Socket } from 'socket.io-client'
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ChatService {
+  private socket: Socket;
+  private url = 'http://localhost:3000';
+
+  constructor() {
+    this.socket = io(this.url, { transports: [ 'websocket', 'pulling', 'flashsocket' ]});
+  }
+
+  joinRoom(data: any): void {
+    this.socket.emit('join', data);
+  }
+
+  sendMessage(data: any): void {
+    this.socket.emit('message', data);
+  }
+
+  getMessage():Observable<any>{
+    return new Observable<{ user: string, message: string }>(observer => {
+      this.socket.on('new message', (data) => {
+        observer.next(data)
+      })
+
+      return () => {
+        this.socket.disconnect()
+      }
+    })
+  }
+
+  getStorage(){
+    const storage: string = localStorage.getItem('chats')
+    return storage ? JSON.parse(storage) : []
+  }
+
+  setStorage(data){
+    localStorage.setItem('chats', JSON.stringify(data))
+  }
+}
