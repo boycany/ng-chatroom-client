76b7876482246233153169bde85d1114520ec106
Red Chien <redchien@viridiasolutions.com>
2023-02-02 16:29:15 +0800

    mobile view working good

---
+++ src/app/data/user-list.ts
@@ -0,0 +1,62 @@
+export const userList: any[] = [
+  {
+    id: 1,
+    name: 'Red',
+    phone: '0961336838',
+    image: 'assets/user/user-1.png',
+    roomId: {
+      2: 'room-1<>2',
+      3: 'room-1<>3',
+      4: 'room-1<>4',
+      5: 'room-1<>5'
+    },
+  },
+  {
+    id: 2,
+    name: 'Red2',
+    phone: '0961336839',
+    image: 'assets/user/user-2.png',
+    roomId: {
+      1: 'room-1<>2',
+      3: 'room-2<>3',
+      4: 'room-2<>4',
+      5: 'room-2<>5'
+    },
+  },
+  {
+    id: 3,
+    name: 'Red3',
+    phone: '0961336837',
+    image: 'assets/user/user-3.png',
+    roomId: {
+      1: 'room-1<>3',
+      2: 'room-2<>3',
+      4: 'room-3<>4',
+      5: 'room-3<>5'
+    },
+  },
+  {
+    id: 4,
+    name: 'Red4',
+    phone: '0961336836',
+    image: 'assets/user/user-4.png',
+    roomId: {
+      1: 'room-1<>4',
+      2: 'room-2<>4',
+      3: 'room-3<>4',
+      5: 'room-4<>5'
+    },
+  },
+  {
+    id: 5,
+    name: 'Red5',
+    phone: '0961336835',
+    image: 'assets/user/user-1.png',
+    roomId: {
+      1: 'room-1<>5',
+      2: 'room-2<>5',
+      3: 'room-3<>5',
+      4: 'room-4<>5'
+    },
+  },
+];
